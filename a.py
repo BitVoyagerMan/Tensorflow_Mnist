857027d20bdb99bf77e66acb880824e48f015d7c
minato <tigerrabbit22@outlook.com>
2023-04-02 11:25:28 +0000

    first

---
+++ a.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+print("Tensorflow version:" , tf.__version__)
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
